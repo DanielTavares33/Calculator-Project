eea6d8c5e93455d92eb73d586b59610548db83ec
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2021-01-18 15:15:50 +0000

    Main Component
    Creation of the main component

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,15 @@
+import './Calculator.css'
+import React, { Component } from 'react'
+
+
+class Calculator extends Component {
+  render() {
+    return (
+      <div className="calculator">
+
+      </div>
+    )
+  }
+}
+
+export default Calculator
