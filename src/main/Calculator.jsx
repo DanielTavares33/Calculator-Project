3009e33f0de606312589d2ee3288806943729a7b
Daniel Tavares <57456238+DanielTavares33@users.noreply.github.com>
2021-01-18 17:13:52 +0000

    Implement Logic #1
    Implementing the logic of adding a digit

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,7 +3,17 @@ import React, { Component } from 'react'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0], // to store the values between operations
+  current: 0 // index of the value in the array
+}
+
 class Calculator extends Component {
+
+  state = { ...initialState }
 
   constructor(props) {
     super(props)
@@ -15,7 +25,7 @@ class Calculator extends Component {
   }
 
   clearMemory() {
-    console.log('limpar')
+    this.setState({ ...initialState })
   }
 
   setOperation(operation) {
@@ -23,13 +33,38 @@ class Calculator extends Component {
   }
 
   addDigit(n) {
-    console.log(n)
+    /* Avoid having two dots on the display */
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    /* 
+      clearDisplay: avoid the left zero with two situations
+        - When the only number displayed is zero
+        - When the state clearDisplay is true
+    */
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n // string concatenation 
+
+    this.setState({ displayValue, clearDisplay: false, })
+
+    if (n !== '.') {
+      const i = this.state.current
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[i] = newValue
+
+      /* when the key have the same name as the value there is no need to do values: values */
+      this.setState({ values })
+      console.log(values)
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100} />
+        <Display value={this.state.displayValue} />
         <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation />
         <Button label="7" click={this.addDigit} />
